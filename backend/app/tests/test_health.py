21c68da53897d45694ac1319b9b2bbd9d50d70f3
pyfig <insober122@mail.ru>
2025-05-05 17:45:53 +0700

    health check

--- backend/app/tests/test_health.py
+++ backend/app/tests/test_health.py
@@ -1,15 +1,8 @@
 import pytest
-from fastapi.testclient import TestClient
-from app.main import app
-
 from httpx import AsyncClient
-from fastapi import FastAPI
-import pytest
-
-from app.main import app  
 
 @pytest.mark.asyncio
-async def test_something():
-    async with AsyncClient(base_url="http://test", app=app) as ac:
-        response = await ac.get("/")
-        assert response.status_code == 200
+async def test_main_root(async_client: AsyncClient):
+    response = await async_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
